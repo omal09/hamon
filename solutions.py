4b42f4f9be3c0f7f02ed270cba789c4e527891a7
omal09 <omal09@users.noreply.github.com>
2021-09-25 03:44:34 +0530

    All problems solved

--- solutions.py
+++ solutions.py
@@ -6,25 +6,32 @@ def biggest(n):
             big = n[i]
     return big
 def biggest2(n):
-    big = [0,0]
+    big2 = [0,0]
     for i in range(len(n)):
-        if n[i]>big[0]:
-            big.pop(0)
-            big.insert(0,n[i])
-            big.sort()
-    return big
+        if n[i]>big2[0]:
+            big2.pop(0)
+            big2.insert(0,n[i])
+            big2.sort()
+    return big2
 def biggestn(n,m):
-    big = [0 for i in range(m)]
+    bign = [0 for i in range(m)]
     for i in range(len(n)):
-        if n[i]>big[0]:
-            big.pop(0)
-            big.insert(0,n[i])
-            big.sort()
-    return big
-
-
-
-
+        if n[i]>bign[0]:
+            bign.pop(0)
+            bign.insert(0,n[i])
+            bign.sort()
+    return bign
+def ffindstring(fname,s):
+    sum = 0
+    with open(fname) as file:
+        for line in file.readlines():
+            for i in s.split("\n"):
+                if i in line:
+                    sum+=1
+        if sum >= len(s.split("\n")):
+            return True
+        else:
+            return False
 def panagram(s):
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     for i in alpha:
@@ -32,4 +39,19 @@ def panagram(s):
             return False
     else:
         return True
-panagram('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+def breakdown(amt,denominations):
+    ans = {}
+    for i,j in denominations.items():
+        if amt>=i:
+            if amt-(i*j)>=0 and j!=0:
+                ans[i]=j
+                amt=amt-(i*j)
+            else:
+                for k in range(1,j):
+                    if amt-(i*k)<i and amt-(i*k)>=0:
+                        ans[i]=k
+                        amt=amt-(i*k)
+    if amt==0:
+        return ans
+    else:
+        return "NotAvailable"
