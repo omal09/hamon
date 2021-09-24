145283ad8f3b6113e67d17f5321bb5b05bbc3d2e
omal09 <omal09@users.noreply.github.com>
2021-09-24 17:43:30 +0530

    4 problems finished

--- solutions.py
+++ solutions.py
@@ -1,1 +1,35 @@
 # Use this file to implement your solutions
+def biggest(n):
+    big = 0
+    for i in range(len(n)):
+        if n[i]>big:
+            big = n[i]
+    return big
+def biggest2(n):
+    big = [0,0]
+    for i in range(len(n)):
+        if n[i]>big[0]:
+            big.pop(0)
+            big.insert(0,n[i])
+            big.sort()
+    return big
+def biggestn(n,m):
+    big = [0 for i in range(m)]
+    for i in range(len(n)):
+        if n[i]>big[0]:
+            big.pop(0)
+            big.insert(0,n[i])
+            big.sort()
+    return big
+
+
+
+
+def panagram(s):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    for i in alpha:
+        if i not in s.lower():
+            return False
+    else:
+        return True
+panagram('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
